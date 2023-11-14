dd80a52349b1706b0c6062ba99efe910970246ff
“MattWoz02git --versiongit config --global user.name “MattWoz02 <mattwozniak02@gmail.com>
2023-11-14 18:53:08 +0000

    Declare Logger

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -2,6 +2,8 @@ import java.util.logging.Logger;
 
 class Dec2Hex
 {
+	// Declare Logger for the Dec2Hex class
+	private static final Logger logger = Logger.getLogger(Dec2Hex.class.getName());
 
     public static int Arg1;
 
