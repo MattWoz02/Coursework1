c132f21d35eaac9f9325078145e420034285773c
“MattWoz02git --versiongit config --global user.name “MattWoz02 <mattwozniak02@gmail.com>
2023-11-14 18:55:08 +0000

    Replace the use of System.out by a logger

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -11,7 +11,7 @@ class Dec2Hex
     {
 	if (args.length == 0)
 	{
-	   System.out.println("Error: No input argument provided. Please provide a valid integer.");
+	   logger.severe("Error: No input argument provided. Please provide a valid integer.");
 	   return;
 	}
 
@@ -19,7 +19,7 @@ class Dec2Hex
 	
 	Arg1 = Integer.parseInt(args[0]);
 	if (Arg1 < 0) {
-                System.out.println("Error: Negative integers are not supported."); 
+                logger.severe("Error: Negative integers are not supported."); 
                 return;
             }
         
@@ -28,7 +28,7 @@ class Dec2Hex
 	int num;
         num = Arg1;
         String hexadecimal="";
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
+        logger.info("Converting the Decimal Value " + num + " to Hex...");
 
         while(num != 0)
 {
@@ -37,9 +37,9 @@ class Dec2Hex
             num= num/16;
         }
 
-        System.out.println("Hexadecimal representation is: " + hexadecimal);
+        logger.info("Hexadecimal representation is: " + hexadecimal);
 	}catch (NumberFormatException e) {
-            System.out.println("Error: Input is not a valid integer. Please provide a decimal number.");
+            logger.severe("Error: Input is not a valid integer. Please provide a decimal number.");
         }
     }
 }
